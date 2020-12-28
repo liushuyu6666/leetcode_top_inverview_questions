25d556a9bf4f870066428db584b2df76b31356d1
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2020-12-28 09:16:54 -0500

    add rotate array

--- java/src/Rotate_array.java
+++ java/src/Rotate_array.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class rotate_array {
+public class Rotate_array {
+    public void rotate(int[] nums, int k){
+        if(k == 0){return;}
+        if(nums.length == 1 || nums.length == k){return;}
+
+        int prevPos, curPos, temp, count = 0;
+        for(int i = nums.length - 1; count < nums.length; i--){
+            curPos = i;
+            while(curPos != (i + k) % nums.length){
+                prevPos = (curPos - k) % nums.length;
+                if(prevPos < 0) prevPos = prevPos + nums.length;
+                temp = nums[prevPos];
+                nums[prevPos] = nums[curPos];
+                nums[curPos] =temp;
+                curPos = prevPos;
+                count ++;
+            }
+            count ++;
+        }
+    }
+    public static void main(String args[]){
+//        int nums1[] = {1, 2, 3, 4, 5, 6};
+        int nums1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11};
+//        int nums1[] = {1, 2, 3, 4, 5, 6};
+        Rotate_array s = new Rotate_array();
+        s.rotate(nums1, 3);
+
+        for(int i = 0; i<nums1.length; i++){
+            System.out.println(nums1[i]);
+        }
+//        System.out.println(s.maxProfit(nums2));
+//        System.out.println(s.maxProfit(nums3));
+    }
 }
