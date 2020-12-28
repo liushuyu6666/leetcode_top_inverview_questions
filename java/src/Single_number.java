d859dd85bfa185baf46327883bc63c82b4470d67
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2020-12-28 09:17:52 -0500

    add single number

--- java/src/Single_number.java
+++ java/src/Single_number.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Single_number {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Single_number {
+    public int singleNumber(int[] nums){
+
+        Set<Integer> set = new HashSet<>(nums.length);
+
+        for(int x:nums){
+            if(set.contains(x)) set.remove(x);
+            else set.add(x);
+        }
+
+        return set.iterator().next();
+    }
+
+    public int singleNumber2(int[] nums){
+
+        // bit manipulation
+        int bit = 0;
+        for(int x:nums){
+            bit ^= x;
+        }
+        return bit;
+    }
+
+    public static void main(String args[]){
+        int[] nums = {3, 4, 3, 5, 4};
+        Single_number s = new Single_number();
+        System.out.println(s.singleNumber2(nums));
+    }
 }
