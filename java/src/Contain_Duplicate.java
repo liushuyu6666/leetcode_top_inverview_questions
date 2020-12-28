f929075693191f964a0042f0dbf2fbf1e569ac72
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2020-12-28 09:15:38 -0500

    add contain duplicate

--- java/src/Contain_Duplicate.java
+++ java/src/Contain_Duplicate.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Contain_Duplicate {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Contain_Duplicate {
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>(nums.length);
+
+        for(int x:nums){
+            if(set.contains(x)) return true;
+            set.add(x);
+        }
+        return false;
+
+    }
+
+    public static void main(String args[]){
+        int[] nums = {1, 2, 3, 4, 4};
+        Contain_Duplicate s = new Contain_Duplicate();
+        System.out.println(s.containsDuplicate(nums));
+    }
 }
