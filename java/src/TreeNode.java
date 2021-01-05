0d80d9b24302c2ef854c65fbf91883fd23c34a61
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-01-05 14:14:30 -0500

    update TreeNode with some facilities

--- java/src/TreeNode.java
+++ java/src/TreeNode.java
@@ -1,3 +1,8 @@
+import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -8,5 +13,83 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    // rain
+    public TreeNode createTreeNode(Integer[] nums){
+        if(nums.length < 1) return null;
+        TreeNode r = new TreeNode();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(r);
+        int i = 0;
+
+        while(i < nums.length){
+
+            // if nums[i] == null, q.remove() must be null as well
+            if(nums[i] == null){
+                i++;
+                q.remove();
+                continue;
+            }
+
+            // get the TreeNode from q
+            TreeNode t = q.remove();
+            int s = q.size();
+            t.val = nums[i].intValue();
+            // add left child node
+            if(i + s + 1 < nums.length && nums[i + s + 1] != null){
+                t.left = new TreeNode();
+                q.add(t.left);
+            }
+            else if(i + s + 1 < nums.length && nums[i + s + 1] == null){
+                q.add(null);
+            }
+            // add right child node
+            if(i + s + 2 < nums.length && nums[i + s + 2] != null){
+                t.right = new TreeNode();
+                q.add(t.right);
+            }
+            else if(i + s + 1 < nums.length && nums[i + s + 2] == null){
+                q.add(null);
+            }
+            i++;
+        }
+        return r;
+    }
+
+    // rain
+    public Queue<Integer> printBreadthFirstSearch(TreeNode root){
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Integer> ans = new LinkedList<>();
+        if(root != null){
+            q.add(root);
+        }
+        else return null;
+
+        while(!q.isEmpty()){
+            TreeNode tn = q.remove();
+            if(tn == null){
+                ans.add(null);
+                continue;
+            }
+            ans.add(new Integer(tn.val));
+            q.add(tn.left);
+            q.add(tn.right);
+        }
+
+        return ans;
+    }
+
+    public static void main(String args[]){
+        Integer[] nums = {5, 1, 4, null, null, 3, 6};
+        TreeNode treeNode = new TreeNode();
+        TreeNode t = treeNode.createTreeNode(nums);
+        Queue<Integer> ans = treeNode.printBreadthFirstSearch(t);
+
+        Integer w;
+        while(!ans.isEmpty()){
+            w = ans.remove();
+            System.out.println(w);
+        }
     }
 }
